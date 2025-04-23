e49085d38218fde8a02b08e27209e92c50299d91
julia0122 <julia.munhoz.22.07@gmail.com>
2025-04-23 10:16:48 -0300

    Add files via upload

---
+++ ritmo.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    float distancia, minutos, segundos;
+    int tempo_total_segundos, ritmo_segunods, ritmo_minutos, ritmo_segundos_finais;
+
+    printf("Digite a distancia percorrida em km: ");
+    scanf("%d", &distancia);
+
+    printf("Digite o tempo gasto em minutos: ");
+    scanf("%d", &minutos);
+
+    printf("Digite o tempo gasto em segundos:");
+    sacnf("%d", segundos);
+
+    tempo_total_segundos = minutos * 60 + segundos;
+
+    ritmo_segundos= tempo_total_segundos / distancia;
+
+    ritmo_minutos= ritmo_segundos / 60;
+    ritmo_segundos_finais = ritmos_segundos % 60;
+
+    printf("Ritmo: %d minutos e %d segundps por quilometro\n", ritmo_minutos, ritmo_segundos_finais);
+
+    return 0;
+}
