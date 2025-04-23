5d9c028455c7555fb1cea4bdc1996d9e619a66d2
julia0122 <julia.munhoz.22.07@gmail.com>
2025-04-23 08:52:02 -0300

    Add files via upload

---
+++ multiplos.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int  numeroBase,inicio, fim, soma=0;
+
+    printf("Digite um numero base: ");
+    scanf("%d", &numeroBase);
+
+    printf("Digite o valor inicial do intervalo:");
+    scanf("%d", &inicio);
+
+    printf("Digite o valor final do intervalo:");
+    scanf("%d", &fim);
+
+    if (inicio>fim)
+    {
+        printf("O valor inicial deve ser menor ou igual ao valor final.\n");
+        return 1;
+    }
+    for (int i=inicio; i<=fim; i++)
+    {
+        if(i% numeroBase==0)
+        {
+            soma+=i;
+        }
+}
