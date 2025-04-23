b0b48d90b1704cd1271337605e1fd04cc9863a65
julia0122 <julia.munhoz.22.07@gmail.com>
2025-04-23 09:39:28 -0300

    Add files via upload

---
+++ casa.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+float base, altura;
+float perimetro, perimetro_rodape, area;
+const float largura_porta=0.8;
+
+printf("base(em metros): ");
+scanf("%f", &base);
+
+printf("altura(em metros): ");
+scanf("%f", &altura);
+
+perimetro= 2*(base+altura);
+perimetro_rodape = perimetro-largura_porta;
+area= base*altura;
+
+printf("\nPerimetro total: %.2f mentros\n", perimetro);
+printf("quantidade necessaria de rodape (descontado a porta): %.2f metros\n", perimetro_rodape);
+printf("area do quarto %.2f metros quadrados\n", area);
+
+return 0;
+}
