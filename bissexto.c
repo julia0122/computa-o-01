151cf8b2d28a20b8e7b8806ae4744ed16603cb5e
julia0122 <julia.munhoz.22.07@gmail.com>
2025-05-07 09:11:10 -0300

    bissexto.c

---
+++ bissexto.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int ehBissexto(int ano){
+if ((ano %4 == 0 && ano % 100 != 0)|| ano %400 == 0){
+return 1;
+}
+else {
+return;
+}
+}
+
+int main () {
+int ano;
+
+printf("Digite um ano: ");
+scanf("%d", &ano);
+
+if (ehBissexto(ano)){
+printf("%d eh um ano bissexto.\n", ano);
+}
+else{
+printf("%d nao eh um ano bissexto.\n", ano);
+}
+
+return 0;
+}
